b71956254f37153152f847b539573625c15a18a5
Wilfried AGO <wilfriedago@pm.me>
2022-11-22 14:26:05 +0100

    Rewrite the main.c file for better readability & clean code.

--- src/main.c
+++ src/main.c
@@ -25,280 +25,149 @@ typedef struct Cellule
 typedef Cellule *Liste;
 
 /**
- * @brief Procédure permettant de nettoyer le contenu de la console
+ * @brief   Function permettant de créer une liste doublement chaînée d'une seul cellule.
+ * @param   valeur Valeur de la première cellule.
+ * @return  *newList La liste nouvellement créer.
  */
-void clearConsole()
-
-#ifdef __unix__
-    system("clear");
-#endif
-}
+Liste createList(int valeur);
 
 /**
- * @brief Procédure permettant de faire une pause la console
+ * @brief Procédure permettant d'ajouter une cellule en tête de liste.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
+ * @param valeur Valeur de la cellule insérer.
  */
-void pauseConsole()
-
-#ifdef __unix__
-    system("sleep 2");
-#endif
-}
+void insertHead(Liste *liste, int valeur);
 
 /**
- * @brief Procédure permettant d'afficher le menu du jeu
+ * @brief Procédure permettant d'ajouter une cellule en queue de liste.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
+ * @param valeur Valeur de la cellule insérer.
  */
-void displayMenu()
+void insertTail(Liste *liste, int valeur);
 
 /**
- * @brief Fonction permettant de lire une caractère saisie à la console
- * @param message Message à afficher dans la console
+ * @brief Procédure permettant de trier une liste.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
  */
-int readNumber(char *message)
+void sortList(Liste *liste);
 
 /**
- * Fonction permettant de vérifier si un nombre est compris dans un intervalle
- * @param number Valeur à tester
- * @param min Valeur minimum
- * @param max Valeur maximale
+ * @brief Fonction permettant de supprimer une cellule de la liste ayant une valeur donné.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
+ * @param valeur Valeur de la cellule à supprimer.
+ * @return true : Si la cellule à été supprimer.
+ * @return false : Si la cellule n'a pas pu être supprimer.
  */
-bool isBetween(int number, int min, int max)
+bool deleteCell(Liste *liste, int valeur);
 
 /**
- * @brief Fonction permettant de vérifier l’existence d'une liste.
- * @param list Liste passé en paramètre par valeur.
+ * @brief Procédure permettant de supprimer les doublons (Cellules ayant la même valeur) dans une liste.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
  */
-bool listExist(Liste list)
+void deleteDuplicates(Liste *liste);
 
 /**
- * @brief Fonction permettant d'initialiser une liste à null.
- * @param list Liste passé en paramètre par variable.
+ * @brief Fonction permettant de supprimer la dernière cellule d'une liste.
+ * @param liste doublement chaînée passée en paramètre par variable.
+ * @return true : Si la cellule a été supprimer.
+ * @return false : Si la cellule n'a pas pu être supprimer.
  */
-void initList(Liste *list)
-{
-    *list = NULL;
-}
+bool deleteLastCell(Liste *liste);
 
 /**
- * @brief Fonction permettant de créer une liste chaînée d'une taille donnée
- * @param list Liste passé en paramètre par variable
- * @param size Taille de la liste
+ * @brief Procédure permettant de fusionner deux listes doublement chaînée.
+ * @param liste1 Première liste à fusionner.
+ * @param liste2 Deuxième liste à fusionner.
  */
-void createList(Liste *list, int size)
-{
-    Liste P = NULL;
-    Liste Q = NULL;
-    int i = 0;
-
-    for (i = 0; i < size; i++)
-    {
-        P = (Liste)malloc(sizeof(Cellule));
-        P->val = readNumber("\nSaisissez un nombre entier : ");
-        P->suiv = NULL;
-        P->prec = NULL;
-
-        if (*list == NULL)
-        {
-            *list = P;
-            Q = P;
-        }
-        else
-        {
-            Q->suiv = P;
-            P->prec = Q;
-            Q = P;
-        }
-    }
-}
+void mergeLists(Liste *liste1, Liste *liste2);
 
 /**
- * @brief Procédure permettant d'afficher une liste
- * @param list Liste passé en paramètre par valeur
+ * @brief Procédure permettant de renverser une liste doublement chaînée.
+ * @param liste Liste chaînée passée en paramètre par variable.
  */
-void printList(Liste list)
-{
-    Liste P = list;
-
-    while (P != NULL)
-    {
-        printf("%d - ", P->val);
-        P = P->suiv;
-    }
-}
+void reverseList(Liste *liste);
 
 /**
- * @brief Procédure permettant d'ajouter un élément en tête de liste
- * @param list Liste passé en paramètre par variable
- * @param value Valeur du nouveau noeud
+ * @brief Fonction permettant de vérifier si une liste est un palindrome.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
  */
-void addHead(Liste *list, int value)
-{
-    Liste P = (Liste)malloc(sizeof(Cellule));
-
-    if (P == NULL)
-        exit(EXIT_FAILURE);
-
-    P->val = value;
-    P->suiv = *list;
-    P->prec = NULL;
-
-    (*list)->prec = P;
-
-    *list = P;
-}
+bool listIsPalindrome(Liste *liste);
 
 /**
- * @brief Procédure permettant d'ajouter un élément en queue de liste
+ * @brief Procédure permettant d'afficher une liste.
+ * @param liste Liste doublement chaînée passé en paramètre par valeur.
  */
-void addTail(Liste *list, int value)
-{
-    Liste P = (Liste)malloc(sizeof(Cellule));
-
-    if (P == NULL)
-        exit(EXIT_FAILURE); // Échec de l'allocation en mémoire
-
-    P->val = value;
-    P->suiv = NULL;
-    P->prec = NULL;
-
-    if (*list == NULL)
-    {
-        *list = P;
-    }
-    else
-    {
-        Cellule *Q = *list;
-        while (Q->suiv != NULL)
-        {
-            Q = Q->suiv;
-        }
-        Q->suiv = P;
-        P->prec = Q;
-    }
-}
+void printList(Liste liste);
 
 /**
- * @brief Procédure permettant de trier une liste
- * @param list Liste passé en paramètre par variable
+ * @brief Function principale du programme.
+ *
+ * @return EXIT_SUCCESS
  */
-void sortList(Liste *list)
-{
-    Cellule *P = *list;
-    Cellule *Q;
-
-    while (P != NULL)
-    {
-        Q = P->suiv;
-        while (Q != NULL)
-        {
-            if (P->val > Q->val)
-            {
-                int temp = P->val;
-                P->val = Q->val;
-                Q->val = temp;
-            }
-            Q = Q->suiv;
-        }
-        P = P->suiv;
-    }
-}
-
-/**
- * @brief Procédure permettant de supprimer un élément donné de la liste
- * @param list Liste passé en paramètre par variable.
- * @param value Valeur de la cellule qu'on veux supprimer.
- */
-bool deleteCell(Liste *list, int value)
-{
-    Cellule *P = *list;
-
-    while (P != NULL)
-    {
-        if (P->val == value)
-        {
-            if (P->prec == NULL) // Suppression à la tête de la liste
-            {
-                *list = P->suiv;
-                P->suiv->prec = NULL;
-            }
-            else if (P->suiv == NULL) // Suppression à la queue de la liste
-            {
-                P->prec->suiv = NULL;
-            }
-            else
-            {
-                P->prec->suiv = P->suiv;
-                P->suiv->prec = P->prec;
-            }
-            free(P);
-            return true; // On retourne vrai si la suppression c'est réalisé
-        }
-        P = P->suiv;
-    }
-    return false; // On retourne faux si il n'y a pas eu de suppression
-}
-
 int main()
 {
-    int choice = 0;
-    int val, size = 0;
-    Liste list = NULL;
+    int choice, val, taille;
+    Liste maListe = NULL;
 
     do
     {
-        // Comment from Codesspaces
         displayMenu();
-        choice = readNumber("\nVotre choix : ");
+        choice = toNumber(readLine("\nVotre choix : "));
 
-        if (isBetween(choice, 0, 11))
+        if (choice >= 0 && choice <= 11)
         {
             switch (choice)
             {
             case 0:
-                if (listExist(list))
-                    printList(list);
+                if (maListe != NULL)
+                    printList(maListe);
                 else
-                    printf("Rien à afficher ! Liste non initialisé !\n");
+                    printf("Rien à afficher ! La liste est vide !\n");
 
                 break;
             case 1:
-                if (listExist(list))
+                if (maListe != NULL)
                 {
-                    printf("Une liste existe déjà !\nCréer une nouvelle liste revient à écraser les valeurs de cette liste existante");
-                    val = readNumber("\n\nVoulez vous vraiment poursuivre cette opération ? Taper 1 pour Oui, ou 0 pour Non : ");
-
-                    if (val == 0)
-                        break; // On passe l'étape si l'utilisateur choisir de ne pas recréer une nouvelle liste.
-                    else if (val == 1)
-                        initList(&list); // On réinitialise la liste si l'utilisateur décide de recréer la liste.
+                    printf("Une liste existe déjà !\nCréer une nouvelle liste revient à écraser la liste existante");
+                    if (readLine("\nVoulez-vous continuer ? (O/N) : ")[0] == 'O')
+                    {
+                        free(maListe);
+                        maListe = NULL;
+                    }
+                    else
+                    {
+                        printf("\nOpération annulée !\n");
+                        break;
+                    }
                 }
-                createList(&list, readNumber("\nTaille de la liste : "));
+
+                taille = toNumber(readLine("\nTaille de la liste : "));
+
+                if (taille > 0)
+                {
+                    maListe = createList(toNumber(readLine("Valeur de la première cellule : ")));
+                    for (int i = 1; i < taille; i++)
+                        insertTail(&maListe, toNumber(readLine("Valeur de la cellule suivante : ")));
+                }
+                else
+                {
+                    printf("La taille de la liste doit être supérieur à 0 !\n");
+                    break;
+                }
+
+                printf("Liste créée avec succès !\n");
                 break;
             case 2:
-                if (listExist(list))
-                    addHead(&list, readNumber("\nValeur à ajouter en tête de liste : "));
-                else
-                    printf("Impossible d'ajouter en tête ! Liste non initialisé !\n");
+
                 break;
             case 3:
-                if (listExist(list))
-                    addTail(&list, readNumber("\nValeur à ajouter en queue de liste : "));
-                else
-                    printf("Impossible d'ajouter en queue ! Liste non initialisé !\n");
+
                 break;
             case 4:
-                if (listExist(list))
-                    sortList(&list);
-                else
-                    printf("Impossible de trier la liste ! Liste non initialisé !\n");
+
                 break;
             case 5:
-                if (listExist(list))
-                {
-                    val = readNumber("\nValeur à supprimer de la liste : ");
-                    printf((deleteCell(&list, val)) ? "\nSuppression effectuée avec succès !" : "\nÉchec de la suppression ! %d ne se trouve pas dans la liste", val);
-                }
-                else
-                    printf("Impossible d'effectué cette opération. Liste non initialisé !\n");
+
                 break;
             case 6:
                 break;
@@ -325,4 +194,196 @@ int main()
     } while (choice != 11);
 
     return EXIT_SUCCESS;
+}
+
+/**
+ * @brief   Function permettant de créer une liste doublement chaînée d'une seul cellule.
+ * @param   valeur Valeur de la première cellule.
+ * @return  *newList La liste nouvellement créer.
+ */
+Liste createList(int valeur)
+{
+    Liste newList = (Liste)malloc(sizeof(Cellule));
+
+    if (newList == NULL)
+    {
+        printf("Erreur d'allocation mémoire !\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newList->val = valeur;
+    newList->suiv = NULL;
+    newList->prec = NULL;
+    return newList;
+}
+
+/**
+ * @brief Procédure permettant d'ajouter une cellule en tête de liste.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
+ * @param valeur Valeur de la cellule insérer.
+ */
+void insertHead(Liste *liste, int valeur)
+{
+    // Cas d'une liste vide.
+    if (*liste == NULL)
+    {
+        (*liste) = createList(valeur);
+        return;
+    }
+
+    Liste newCell = (Liste)malloc(sizeof(Cellule));
+
+    if (newCell == NULL)
+    {
+        printf("Erreur d'allocation mémoire !\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newCell->val = valeur;
+    newCell->suiv = *liste;
+    newCell->prec = NULL;
+
+    (*liste)->prec = newCell;
+    *liste = newCell;
+}
+
+/**
+ * @brief Procédure permettant d'ajouter une cellule en queue de liste.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
+ * @param valeur Valeur de la cellule insérer.
+ */
+void insertTail(Liste *liste, int valeur)
+{
+    // Cas d'une liste vide.
+    if (*liste == NULL)
+    {
+        (*liste) = createList(valeur);
+        return;
+    }
+
+    Liste newCell = (Liste)malloc(sizeof(Cellule));
+
+    if (newCell == NULL)
+    {
+        printf("Erreur d'allocation mémoire !\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newCell->val = valeur;
+    newCell->suiv = NULL;
+
+    Liste tempCell = *liste;
+
+    while (tempCell->suiv != NULL)
+        tempCell = tempCell->suiv;
+
+    tempCell->suiv = newCell;
+    newCell->prec = tempCell;
+}
+
+/**
+ * @brief Procédure permettant de trier une liste.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
+ */
+void sortList(Liste *liste)
+{
+    Liste tempCell = *liste;
+    int tempVal;
+
+    while (tempCell != NULL)
+    {
+        if (tempCell->suiv != NULL)
+        {
+            if (tempCell->val > tempCell->suiv->val)
+            {
+                tempVal = tempCell->val;
+                tempCell->val = tempCell->suiv->val;
+                tempCell->suiv->val = tempVal;
+            }
+        }
+
+        tempCell = tempCell->suiv;
+    }
+}
+
+/**
+ * @brief Fonction permettant de supprimer une cellule de la liste ayant une valeur donné.
+ * @param liste Liste doublement chaînée passée en paramètre par variable.
+ * @param valeur Valeur de la cellule à supprimer.
+ * @return true : Si la cellule à été supprimer.
+ * @return false : Si la cellule n'a pas pu être supprimer.
+ */
+bool deleteCell(Liste *liste, int valeur)
+{
+    Liste tempCell = *liste;
+
+    while (tempCell != NULL)
+    {
+        if (tempCell->val == valeur)
+        {
+            if (tempCell->prec == NULL) // Cas où la cellule à supprimer est en tête de liste.
+            {
+                *liste = tempCell->suiv;
+                (*liste)->prec = NULL;
+            }
+
+            else if (tempCell->suiv == NULL) // Cas où la cellule à supprimer est en queue de liste.
+                tempCell->prec->suiv = NULL;
+
+            else // Cas où la cellule à supprimer se trouve  dans la liste.
+            {
+                tempCell->prec->suiv = tempCell->suiv;
+                tempCell->suiv->prec = tempCell->prec;
+            }
+
+            free(tempCell);
+            return true;
+        }
+
+        tempCell = tempCell->suiv;
+    }
+
+    return false;
+}
+
+/**
+ * @brief Procédure permettant de supprimer les doublons d'une liste
+ * @param liste Liste passé en paramètre par variable
+ */
+// TODO : Revoir la logique.
+void deleteDuplicates(Liste *liste)
+{
+    Liste tempCell = *liste;
+
+    while (tempCell != NULL)
+    {
+        if (tempCell->suiv != NULL)
+        {
+            if (tempCell->val == tempCell->suiv->val)
+            {
+                if (tempCell->suiv->suiv != NULL)
+                {
+                    tempCell->suiv = tempCell->suiv->suiv;
+                    tempCell->suiv->prec = tempCell;
+                }
+                else
+                    tempCell->suiv = NULL;
+            }
+        }
+
+        tempCell = tempCell->suiv;
+    }
+}
+
+/**
+ * @brief Procédure permettant d'afficher une liste.
+ * @param liste Liste doublement chaînée passé en paramètre par valeur.
+ */
+void printList(Liste liste)
+{
+    while (liste != NULL)
+    {
+        printf("%d\t", liste->val);
+        printList(liste->suiv);
+    }
 }
